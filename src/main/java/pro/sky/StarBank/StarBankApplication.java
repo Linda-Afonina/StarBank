52b1e870f0329e3c2250bc6cb10dd2f55f86057e
linda <charming.linda85@gmail.com>
2024-10-15 20:29:01 +0300

    Initial commit

---
+++ src/main/java/pro/sky/StarBank/StarBankApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.StarBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/StarBank/StarBankApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.StarBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
