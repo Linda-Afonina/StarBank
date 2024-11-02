b223f45969ba3924ab34a803503a12d1cae8002a
linda <charming.linda85@gmail.com>
2024-11-02 14:27:41 +0300

    Добавлено кэширование и его проверка в Сервисе.

--- src/main/java/pro/sky/StarBank/service/DynamicRulesService.java
+++ src/main/java/pro/sky/StarBank/service/DynamicRulesService.java
@@ -1,5 +1,7 @@
 package pro.sky.StarBank.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import pro.sky.StarBank.model.DynamicRules;
 import pro.sky.StarBank.repository.DynamicRulesRepository;
@@ -7,10 +9,13 @@ import pro.sky.StarBank.repository.DynamicRulesRepository;
 import java.util.List;
 import java.util.Map;
 
+
 @Service
 public class DynamicRulesService {
 
     private final DynamicRulesRepository dynamicRulesRepository;
+
+    Logger logger = LoggerFactory.getLogger(DynamicRulesService.class);
 
     public DynamicRulesService(DynamicRulesRepository dynamicRulesRepository) {
         this.dynamicRulesRepository = dynamicRulesRepository;
@@ -25,6 +30,11 @@ public class DynamicRulesService {
     }
 
     public List<Map<String, Object>> getAllRules() {
-        return dynamicRulesRepository.getAllRules();
+        long startTime = System.currentTimeMillis();
+        List<Map<String, Object>> list = dynamicRulesRepository.getAllRules();
+        long endTime = System.currentTimeMillis();
+        logger.info("Start time: " + startTime + ", end time: " + endTime + ", operation time: "
+                + (endTime - startTime) + " ms.");
+        return list;
     }
 }
